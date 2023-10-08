39d803ed0d22d8efe8be011a8aca8e546e703cbd
owner <darknesscrazyman@hotmail.com>
2023-10-08 18:36:39 +0100

    Changed everything

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ app = Flask(__name__)
 app.secret_key = "gandc-wedding-key"
 
 
-@app.route("/rsvp")
-def index():
+@app.route("/")
+def base():
+    return render_template("home.html")
+
+
+@app.route("/rsvp", methods=["POST", "GET"])
+def rsvp():
     flash("Please RSVP below")
-    return render_template("index.html")
-
-
-@app.route("/other-shit", methods=["POST", "GET"])
-def greeter():
-    flash("Hi " + str(request.form['name_input']) + ", great to see you!")
-    return render_template("index.html")
+    return render_template("rsvp.html")
+#
+# @app.route("/other-shit", methods=["POST", "GET"])
+# def greeter():
+#     flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+#     return render_template("base.html")
