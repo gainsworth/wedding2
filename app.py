33a8c78bd0f499796d4add2a295480bd48ece0b9
owner <darknesscrazyman@hotmail.com>
2023-11-18 21:08:35 +0000

    Added email column and fixed bugs

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
 from datetime import datetime
@@ -38,7 +39,6 @@ def generate_csv_attachment(model, headers, filename):
 
 
 def send_george_email(name, party_string, attach_csvs=True):
-
     # Create a multipart message
     msg = MIMEMultipart()
     msg['Subject'] = 'See you at the wedding!'
@@ -55,11 +55,13 @@ def send_george_email(name, party_string, attach_csvs=True):
     if attach_csvs:
         # Attach AllEntries CSV
         all_entries_headers = ["id", "first_name", "last_name", "email", "time_of_entry"]
+        # noinspection PyTypeChecker
         all_entries_part = generate_csv_attachment(AllEntries, all_entries_headers, "all_entries.csv")
         msg.attach(all_entries_part)
 
         # Attach RSVP CSV
         rsvp_headers = ["id", "guest_id", "attending", "updated_first_name", "updated_last_name"]
+        # noinspection PyTypeChecker
         rsvp_part = generate_csv_attachment(RSVP, rsvp_headers, "rsvp.csv")
         msg.attach(rsvp_part)
 
@@ -77,7 +79,6 @@ app = Flask(__name__)
 
 
 def send_email(name, party_string, email):
-
     with open('templates/thank_you_email.html', encoding='utf8') as infile:
         email_html = infile.read()
     body = email_html.format(name, party_string)
@@ -94,11 +95,14 @@ def send_email(name, party_string, email):
     server.send_message(msg)
     server.quit()
 
+
 # Use SQLite for simplicity
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rsvp.db'
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 
 class Guest(db.Model):
@@ -119,6 +123,7 @@ class RSVP(db.Model):
     attending = db.Column(db.Boolean, nullable=False)
     updated_first_name = db.Column(db.String, nullable=False)
     updated_last_name = db.Column(db.String, nullable=True)
+    email = db.Column(db.String, nullable=False)
 
 
 class AllEntries(db.Model):
@@ -129,7 +134,7 @@ class AllEntries(db.Model):
     time_of_entry = db.Column(db.String, nullable=True)
 
 
-@app.route("/shit_home_page")
+@app.route("/home")
 def index():
     return render_template("index.html")
 
@@ -149,7 +154,7 @@ def rsvp():
     if request.method == 'POST':
         first_name = request.form['first_name'].title().strip()
         last_name = request.form['last_name'].title().strip()
-        email = request.form['email']
+        email = request.form['email'].strip()
 
         new_entry = AllEntries(first_name=first_name, last_name=last_name,
                                email=email, time_of_entry=str(datetime.now()))
@@ -173,6 +178,7 @@ def rsvp():
                                               Guest.enterable != 'No').first()
         guest = guest_search
         if guest:
+            main_id = guest.id
             family_members = Guest.query.filter_by(family_id=guest.family_id).all()
             if len(family_members) == 1:
                 if [x.email for x in AllEntries.query.all()].count(email) == 1 \
@@ -180,18 +186,25 @@ def rsvp():
                     send_email(first_name, 'you', email)
                     send_george_email(first_name, 'you', attach_csvs=True)
                 new_rsvp = RSVP(guest_id=guest.id, attending=True,
-                                updated_first_name=first_name, updated_last_name=last_name)
+                                updated_first_name=first_name, updated_last_name=last_name, email=email)
                 db.session.add(new_rsvp)
-                return render_template('thank_you.html', party_string='you')
-            return render_template('rsvp_form.html', family_members=family_members, email=email)
+                db.session.commit()
+                return render_template('thank_you.html', party_string='you', first_name=first_name)
+            else:
+                # main_guest = Guest(number=guest.number, first_name=first_name, last_name=last_name, enterable='Yes',
+                #                    assume_last_name='', last_name_searchable='', alternative_first_name='',
+                #                    family_id=guest.family_id)
+                guest.first_name = first_name
+                guest.last_name = last_name
+                return render_template('rsvp_form.html', family_members=family_members, email=email, main_id=main_id)
         else:
             if [x.email for x in AllEntries.query.all()].count(email) == 1 or email == 'darknesscrazyman@hotmail.com':
                 send_email(first_name, 'you', email)
                 send_george_email(first_name, 'you', attach_csvs=True)
-            new_rsvp = RSVP(guest_id=-1, attending=True,
-                            updated_first_name=first_name, updated_last_name=last_name)
-            db.session.add(new_rsvp)
-            return render_template('thank_you.html', party_string='you')
+            # new_rsvp = RSVP(guest_id=-1, attending=True,
+            #                 updated_first_name=first_name, updated_last_name=last_name)
+            # db.session.add(new_rsvp)
+            return render_template('thank_you.html', party_string='you', first_name=first_name)
             # return redirect(url_for('thank_you', party_string='you'))
 
     return render_template('rsvp_initial.html')
@@ -200,6 +213,7 @@ def rsvp():
 @app.route('/submit_rsvp', methods=['POST'])
 def submit_rsvp():
     party = []
+    main_id = request.form['main_id']
     for key in request.form:
         if key.startswith('attending_'):
             member_id = int(key.split('_')[1])
@@ -208,7 +222,11 @@ def submit_rsvp():
             updated_last_name = request.form.get('last_name_{}'.format(member_id))
 
             if attending:
-                party.append(updated_first_name)
+                print(member_id, main_id, updated_first_name, member_id == main_id, type(member_id), type(main_id))
+                party.append(updated_first_name if member_id != int(main_id) else 'you')
+                if member_id == main_id:
+                    name = updated_first_name
+                print(party)
 
             new_rsvp = RSVP(guest_id=member_id, attending=attending,
                             updated_first_name=updated_first_name, updated_last_name=updated_last_name)
@@ -217,12 +235,13 @@ def submit_rsvp():
     db.session.commit()
 
     email = request.form['email']
-    party_string = ', '.join(['you', *party[1:-1]]) + f'{" and " + party[-1] if len(party) > 1 else ""}'
+    party.sort(key=lambda k: 0 if k == 'you' else 1)
+    party_string = ', '.join(party[:-1]) + f'{" and " + party[-1] if len(party) > 1 else ""}'
     if [x.email for x in AllEntries.query.all()].count(email) == 1 or email == 'darknesscrazyman@hotmail.com':
         send_email(party[0], party_string, email)
         send_george_email(party[0], party_string, attach_csvs=True)
 
-    return render_template('thank_you.html', party_string=party_string)
+    return render_template('thank_you.html', party_string=party_string, first_name=name)
     # return redirect(url_for('thank_you', party_string=party_string))
 
 
